826d46c19b73141a15757b5dea524fa6d73e214d
Felipe <pipecalgaro@gmail.com>
2022-09-04 16:40:53 -0300

    create login page and connect to api

--- src/services/users.js
+++ src/services/users.js
@@ -1,7 +1,8 @@
 import Api from './api'
 
 const UsersService = {
-  register: (params) => Api.post('/users/register', params)
+  register: (params) => Api.post('/users/register', params),
+  login: (params) => Api.post('/users/login', params)
 }
 
 export default UsersService
