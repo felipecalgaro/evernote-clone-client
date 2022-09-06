21f5ab8a58efce7be3f7357a3de3a113a85bcfd6
Felipe <pipecalgaro@gmail.com>
2022-09-06 14:55:57 -0300

    create notes page

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,11 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import HeaderLogged from "../../../components/header_logged";
+import Notes from '../../../components/notes';
 
-const Notes = () => (
-  <Fragment>
-    <HeaderLogged />
-    Hello World
-  </Fragment>
-);
+const NotesScreen = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-export default Notes;
+  return (
+    <Fragment>
+      <HeaderLogged setIsOpen={setIsOpen} />
+      <Notes setIsOpen={setIsOpen} isOpen={isOpen} />
+    </Fragment>
+  )
+}
+
+export default NotesScreen;
