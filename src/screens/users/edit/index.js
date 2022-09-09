793c217246d0f4c200b1cf4af0cd1caf3ef47705
Felipe <pipecalgaro@gmail.com>
2022-09-09 19:22:59 -0300

    add update name and email functionality

--- src/screens/users/edit/index.js
+++ src/screens/users/edit/index.js
@@ -1,9 +1,47 @@
 import React, { Fragment } from 'react';
+import { Column, Section, Title, Container, Card, Button } from "rbx";
+import "../../../styles/users.scss";
+import HeaderLogged from "../../../components/header_logged";
+import UsersEditForm from '../../../components/users/user_edit_form';
 
 const UserEdit = () => (
-  <>
-    Hello World
-  </>
+  <Fragment>
+    <HeaderLogged />
+    <Section size="medium" className="users">
+      <Container>
+        <Column.Group centered className="users-edit">
+          <Column size={4}>
+            <Title size={5} className="has-text-grey has-text-left">
+              Informações Pessoais
+            </Title>
+            <Card>
+              <Card.Content>
+                <UsersEditForm />
+              </Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+
+        <Column.Group centered className="users-edit">
+          <Column size={4}>
+            <Title size={5} className="has-text-grey has-text-left">
+              Password
+            </Title>
+            <Card>
+              <Card.Content>
+                Users Edit Password Form...
+              </Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+        <Column.Group centered>
+          <Column size={4} className="has-text-right">
+            Users Delete Button...
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
+  </Fragment>
 );
 
 export default UserEdit;
